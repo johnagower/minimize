e0974b8211e48f6ca47195b20669915e88a30959
johnagower <johnagower@gmail.com>
2017-12-02 17:37:08 -0800

    Done

--- inventory/views.py
+++ inventory/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render (request, 'index.html')
+	number = 6
+	name = "John"
+	return render (request, 'index.html', {
+		'number': number,
+		'name': name,
+	})
