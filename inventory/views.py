a3402423479eb5815248a9ea3a4474cbf6e53b03
johnagower <johnagower@gmail.com>
2017-12-04 03:29:03 -0800

    Done

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.template.defaultfilters import slugify
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from inventory.forms import ThingForm
 from inventory.models import Thing
 
@@ -17,8 +20,11 @@ def thing_detail(request, slug):
 		'thing': thing,
 	})
 
+@login_required
 def edit_thing(request, slug):
 	thing = Thing.objects.get(slug=slug)
+	if thing.user != request.user:
+		raise Http404
 	form_class = ThingForm
 	if request.method == 'POST':
 		form = form_class(data=request.POST, instance=thing)
@@ -30,5 +36,21 @@ def edit_thing(request, slug):
 		
 	return render(request, 'things/edit_thing.html', {
 		'thing': thing,
+		'form': form,
+	})
+
+def create_thing(request):
+	form_class = ThingForm
+	if request.method == 'POST':
+		form = form_class(request.POST)
+		if form.is_valid():
+			thing = form.save(commit=False)
+			thing.user = request.user
+			thing.slug = slugify(thing.name)
+			thing.save()
+			return redirect('thing_detail', slug=thing.slug)
+	else:
+		form = form_class()
+	return render(request, 'things/create_thing.html', {
 		'form': form,
 	})
--- mystuff/urls.py
+++ mystuff/urls.py
@@ -20,9 +20,12 @@ from django.contrib.auth.views import (
 	password_reset_done,
 	password_reset_confirm,
 	password_reset_complete,
+	password_change,
+	password_change_done,
 )
 from django.views.generic import TemplateView
 from inventory import views
+from inventory.backends import MyRegistrationView
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
@@ -36,6 +39,11 @@ urlpatterns = [
     	name='thing_detail'),
     url(r'^things/(?P<slug>[-\w]+)/edit/$',
     	views.edit_thing, name='edit_thing'),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(),
+    	name='registration_register'),
+    url(r'^accounts/create_thing/$',
+    	views.create_thing,
+    	name='registration_create_thing'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^accounts/password/reset/$', password_reset,
     	{'template_name': 'registration/password_reset_form.html'},
@@ -52,5 +60,13 @@ urlpatterns = [
     	password_reset_complete,
     	{'template_name': 'registration/password_reset_complete.html'},
     	name="password_reset_complete"),
+    url(r'^accounts/password/change/$',
+    	password_change,
+    	{'template_name': 'registration/password_change_form.html'},
+    	name="password_change"),
+    url(r'^accounts/password/change/done/$',
+    	password_change_done,
+    	{'template_name': 'registration/password_change_done.html'},
+    	name="password_change_done"),
     url(r'^admin/', admin.site.urls),
 ]
