e0eee1374bebc41128855fa8b2df372c1bbd7636
johnagower <johnagower@gmail.com>
2018-03-22 19:09:46 -0700

    trying to fix static image thing again

--- mystuff/settings.py
+++ mystuff/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_FILES_DIRS = (
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
 )
 
